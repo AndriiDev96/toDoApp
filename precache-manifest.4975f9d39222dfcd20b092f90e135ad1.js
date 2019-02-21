042f4819fb3bc7492223b7e38de7eff03cd79a77
AndriiDev96 <volyanskiy.2015@gmail.com>
2019-02-21 21:23:13 +0200

    Updates

---
+++ precache-manifest.4975f9d39222dfcd20b092f90e135ad1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/toDoApp/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "e05936bfdbda9b38e556",
+    "url": "/toDoApp/static/css/main.c1bca36b.chunk.css"
+  },
+  {
+    "revision": "c75f7ed735d4438ecbf1",
+    "url": "/toDoApp/static/js/runtime~main.c75f7ed7.js"
+  },
+  {
+    "revision": "d2aabf5297a4cd5d42897132fe9cc09e",
+    "url": "/toDoApp/static/media/app_top.d2aabf52.png"
+  },
+  {
+    "revision": "d1ecc57f45f65eba74e9",
+    "url": "/toDoApp/static/js/2.d1ecc57f.chunk.js"
+  },
+  {
+    "revision": "e05936bfdbda9b38e556",
+    "url": "/toDoApp/static/js/main.e05936bf.chunk.js"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/toDoApp/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "08e9eaf583c67ca500b3e36c43af2eb7",
+    "url": "/toDoApp/static/media/goluboy_fon.08e9eaf5.png"
+  },
+  {
+    "revision": "6267f50d8327cc00b486719ecfaedfd6",
+    "url": "/toDoApp/static/media/goluboy_fon_mobile.6267f50d.jpg"
+  },
+  {
+    "revision": "d1ecc57f45f65eba74e9",
+    "url": "/toDoApp/static/css/2.5366cea9.chunk.css"
+  },
+  {
+    "revision": "afac4d1e630ccc10d76eedb0b058f708",
+    "url": "/toDoApp/index.html"
+  }
+];
